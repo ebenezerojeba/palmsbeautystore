edf88db8fea35b1276608d4d5f876dd70c82d841
ebenezerojeba <69347304+ebenezerojeba@users.noreply.github.com>
2025-02-10 21:51:45 +0100

    U

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
 import AdminContextProvider from "./context/adminContext";
+// import AdminContextProvider from "./context/adminContext";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
