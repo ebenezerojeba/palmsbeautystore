07d01ea3d235b9f5f32e70240d44375771cb0eed
ebenezerojeba <69347304+ebenezerojeba@users.noreply.github.com>
2025-02-15 13:56:28 +0100

    U

--- admin/src/context/AdminContexts.jsx
+++ admin/src/context/AdminContexts.jsx
@@ -228,8 +228,8 @@ const AdminContextsProvider = (props) => {
   });
 
   // const backendUrl = import.meta.env.VITE_BACKEND_URL
-  const backendUrl = "http://localhost:3000";
-//   const backendUrl = "https://palmsbeautystore-backend.onrender.com"
+//   const backendUrl = "http://localhost:3000";
+  const backendUrl = "https://palmsbeautystore-backend.onrender.com"
 
   const months = [
     "",
