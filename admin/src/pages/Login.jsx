e579cdd9579510e9d27f418c2602de2838903f45
ebenezerojeba <69347304+ebenezerojeba@users.noreply.github.com>
2025-07-13 20:11:58 +0100

    U

--- admin/src/pages/Login.jsx
+++ admin/src/pages/Login.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import React, { useState } from 'react'
 import { toast } from 'react-toastify'
+import { Loader2 } from "lucide-react";
 
 
 const backendUrl = "https://palmsbeautystore-backend.onrender.com"
@@ -8,9 +9,11 @@ const backendUrl = "https://palmsbeautystore-backend.onrender.com"
 const Login = ({setToken}) => {
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
+    const [isLoading, setIsLoading] = useState(false)
     // const {backendUrl} = useContext(AdminContexts);
     const onSubmitHandler = async (e) => {
         try {
+             setIsLoading(true)
             e.preventDefault();
             const response = await axios.post(backendUrl + "/api/user/admin", {email,password})
             if(response.data.success) {
@@ -23,6 +26,9 @@ else{
             console.log(error);
             toast.error(error.message)
             
+        }
+        finally {
+            setIsLoading(false)
         }
     }
   return (
@@ -38,7 +44,8 @@ else{
                         <p className='font-sm font-medium text-gray-700 mb-2'>Password</p>
                     <input onChange={(e) => setPassword(e.target.value)} value={password} className='rounded-md w-full px-3 py-2 border border-gray-300 outline-none' type="password" name="" placeholder='Enter your password' id="" />
                 </div>
-                <button className='mt-2 w-full py-2 px-4 rounded-md text-white bg-black' type='submit'>Login</button>
+                <button className='mt-2 w-full py-2 px-4 rounded-md text-white bg-black' type='submit' disabled={isLoading}>
+                    {isLoading ? <Loader2 />: "Login"}</button>
             </form>
         </div>
     </div>
