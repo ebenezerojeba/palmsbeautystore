c3702d29d5bb4bececc64b7fcad2347d4945cf19
ebenezerojeba <69347304+ebenezerojeba@users.noreply.github.com>
2025-07-13 06:54:08 +0100

    U

--- frontend/src/constants/categories.js
+++ frontend/src/constants/categories.js
@@ -3,6 +3,6 @@ export const categories = {
   Skincare: ["Cleansers", "Moisturizers", "Serums", "Sunscreens", "Treatments"],
   Makeup: ["Foundation", "Lipstick", "Eyeshadow", "Mascara", "Blush"],
   Fragrance: ["Perfume", "Cologne", "Body Spray", "Essential Oils"],
-  HairCare: ["Shampoo", "Conditioner", "Styling", "Treatments"],
+  Haircare: ["Shampoo", "Conditioner", "Styling", "Treatments"],
   Bodycare: ["Lotions", "Scrubs", "Oils", "Cleansers"],
 };
