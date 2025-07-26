05a225891fff068c3aeae7a98f3b1731a85e80f0
ebenezerojeba <69347304+ebenezerojeba@users.noreply.github.com>
2025-07-26 11:53:13 +0100

    U

--- backend/server.js
+++ backend/server.js
@@ -82,6 +82,11 @@ app.get("/", (req, res) => {
   res.send("API Working Perfctly");
 });
 
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+});
+
+
 app.listen(port, () => {
   console.log(`Server started on port: ${port}`);
 });
