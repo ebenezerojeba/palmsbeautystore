cf40e936533b7ee046ef76e886e26ffffad7a66f
ebenezerojeba <69347304+ebenezerojeba@users.noreply.github.com>
2025-07-28 11:49:35 +0100

    U

--- admin/src/context/AdminContexts.jsx
+++ admin/src/context/AdminContexts.jsx
@@ -18,7 +18,8 @@ const [loadingId, setLoadingId] = useState(null);
 
  
   // const backendUrl = "https://palmsbeauty-backend.vercel.app";
-  const backendUrl = "https://palmsbeautystore-backend.onrender.com"
+  // const backendUrl = "https://palmsbeautystore-backend.onrender.com"
+  const backendUrl = "http://localhost:3000"
 
   const months = [
     "",
