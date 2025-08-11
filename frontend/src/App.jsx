e30b3d73f108c791649e735ec6470bfcfe160254
ebenezerojeba <69347304+ebenezerojeba@users.noreply.github.com>
2025-08-12 00:01:05 +0100

    U

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,137 +1,74 @@
-// import React from 'react'
-// import { Route, Routes } from 'react-router-dom'
-// import Navbar from './components/Navbar'
-// import Services from './pages/Services'
-// import About from './pages/About'
-// import Contact from './pages/Contact'
-// import Home from './pages/Home'
-// import Footer from './components/Footer'
-// import Appointment from './pages/Appointment'
-// import MyAppointment from './pages/MyAppointment'
+// import React from 'react';
+// import { Route, Routes } from 'react-router-dom';
+// import Navbar from './components/Navbar';
+// import Footer from './components/Footer';
 // import { ToastContainer } from 'react-toastify';
-// import Product from './pages/Product'
-// import Cart from './pages/Cart'
-// import PlaceOrder from './pages/PlaceOrder'
-// import Collection from './pages/Collection'
-// import ServicesPage from './pages/ServicesPage'
-// import Login from './pages/Login'
-// import MyProfile from './pages/MyProfile'
-// import Orders from './pages/MyOrders'
-// import VerifyAppointment from './pages/VerifyAppointment'
-// import SuccessPage from './pages/SuccessPage'
 
-
+// import Home from './pages/Home';
+// import About from './pages/About';
+// import Contact from './pages/Contact';
+// import Services from './pages/Services';
+// import ServicesPage from './pages/ServicesPage';
+// import Appointment from './pages/Appointment';
+// import MyAppointment from './pages/MyAppointment';
+// import Product from './pages/Product';
+// import Cart from './pages/Cart';
+// import PlaceOrder from './pages/PlaceOrder';
+// import Collection from './pages/Collection';
+// import Login from './pages/Login';
+// import MyProfile from './pages/MyProfile';
+// import Orders from './pages/MyOrders';
+// import VerifyAppointment from './pages/VerifyAppointment';
+// import SuccessPage from './pages/SuccessPage';
 
 // const App = () => {
 //   return (
-//     <div className=''>
+//     <div className="min-h-screen flex flex-col bg-white text-gray-900">
 //       <ToastContainer />
 //       <Navbar />
-//       <div className='mx-4 sm:mx-[-2%]'>
 
+//       {/* Main content wrapper */}
+//       <main className="flex-grow">
+//         <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-6">
+//           <Routes>
+//             <Route path="/" element={<Home />} />
+//             <Route path="/login" element={<Login />} />
+//             <Route path="/services" element={<Services />} />
+//             <Route path="/servicespage" element={<ServicesPage />} />
+//             <Route path="/about" element={<About />} />
+//             <Route path="/contact" element={<Contact />} />
+//             <Route path="/collections" element={<Collection />} />
+//             <Route path="/appointment/:id" element={<Appointment />} />
+//             <Route path="/appointment/verify/:appointmentId" element={<VerifyAppointment />} />
+//             <Route path="/success/:appointmentId" element={<SuccessPage />} />
+//             <Route path="/product/:productId" element={<Product />} />
+//             <Route path="/cart" element={<Cart />} />
+//             <Route path="/place-order" element={<PlaceOrder />} />
+//             <Route path="/my-profile" element={<MyProfile />} />
+//             <Route path="/my-orders" element={<Orders />} />
+//             <Route path="/my-appointments" element={<MyAppointment />} />
+//           </Routes>
+//         </div>
+//       </main>
 
-//         <Routes>
-//           <Route path="/" element={<Home />} />
-//           <Route path="/login" element={<Login />} />
-
-//           <Route path="/services" element={<Services />} />
-//           <Route path="/servicespage" element={<ServicesPage />} />
-//           <Route path="/about" element={<About />} />
-//           {/* <Route path="/cart" element={<Cart />} /> */}
-
-//           <Route path="/contact" element={<Contact />} />
-//           <Route path="/collections" element={<Collection />} />
-//           <Route path="/appointment/:id" element={<Appointment />} />
-          
-//           <Route
-//             path="/appointment/verify/:appointmentId"
-//             element={<VerifyAppointment />}
-//           />
-          
-// <Route path="/success/:appointmentId" element={<SuccessPage />} />
-
-//           {/* <Route path="/success/:appointmentId" element={<SuccessPage />} /> */}
-
-//           <Route path='/product/:productId' element={<Product />} />
-//           <Route path='/cart' element={<Cart />} />
-//           <Route path='/place-order' element={<PlaceOrder />} />
-
-//           <Route path='/my-profile' element={<MyProfile />} />
-//           <Route path='/my-orders' element={<Orders />} />
-//           <Route path="/my-appointments" element={<MyAppointment />} />
-//         </Routes>
-//       </div>
 //       <Footer />
 //     </div>
-//   )
-// }
+//   );
+// };
 
-// export default App
-
-
+// export default App;
 
 
 
 
 
 
-
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import { ToastContainer } from 'react-toastify';
-
-import Home from './pages/Home';
-import About from './pages/About';
-import Contact from './pages/Contact';
-import Services from './pages/Services';
-import ServicesPage from './pages/ServicesPage';
-import Appointment from './pages/Appointment';
-import MyAppointment from './pages/MyAppointment';
-import Product from './pages/Product';
-import Cart from './pages/Cart';
-import PlaceOrder from './pages/PlaceOrder';
-import Collection from './pages/Collection';
-import Login from './pages/Login';
-import MyProfile from './pages/MyProfile';
-import Orders from './pages/MyOrders';
-import VerifyAppointment from './pages/VerifyAppointment';
-import SuccessPage from './pages/SuccessPage';
+import React from 'react'
 
 const App = () => {
   return (
-    <div className="min-h-screen flex flex-col bg-white text-gray-900">
-      <ToastContainer />
-      <Navbar />
+    <div>App</div>
+  )
+}
 
-      {/* Main content wrapper */}
-      <main className="flex-grow">
-        <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-6">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/services" element={<Services />} />
-            <Route path="/servicespage" element={<ServicesPage />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/collections" element={<Collection />} />
-            <Route path="/appointment/:id" element={<Appointment />} />
-            <Route path="/appointment/verify/:appointmentId" element={<VerifyAppointment />} />
-            <Route path="/success/:appointmentId" element={<SuccessPage />} />
-            <Route path="/product/:productId" element={<Product />} />
-            <Route path="/cart" element={<Cart />} />
-            <Route path="/place-order" element={<PlaceOrder />} />
-            <Route path="/my-profile" element={<MyProfile />} />
-            <Route path="/my-orders" element={<Orders />} />
-            <Route path="/my-appointments" element={<MyAppointment />} />
-          </Routes>
-        </div>
-      </main>
-
-      <Footer />
-    </div>
-  );
-};
-
-export default App;
+export default App
