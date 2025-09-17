643e9198145d2d16e0515f883eb56fe0c1e7ee15
ebenezerojeba <69347304+ebenezerojeba@users.noreply.github.com>
2025-09-17 06:10:14 +0100

    U

--- backend/server.js
+++ backend/server.js
@@ -173,18 +173,9 @@ const allowedOrigins = [
 ];
 
 const corsOptions = {
-  origin: function (origin, callback) {
-    // Allow requests with no origin (like mobile apps, Postman, etc.)
-    if (!origin) return callback(null, true);
-    
-    if (allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      console.log('Blocked by CORS:', origin);
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+origin: true,
   credentials: true,
+
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
   allowedHeaders: [
     'Origin',
